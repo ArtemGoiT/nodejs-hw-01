9b3c1f476eadc665caa983182517e34479698834
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-07-16 22:38:45 +0200

    fixx

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,26 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const newContactsPush = [];
+    for (let i = 0; i < number; i++) {
+      newContactsPush.push(createFakeContact());
+    }
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    const updatedContacts = [...contacts, ...newContactsPush];
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContacts, null, 2),
+      'utf-8',
+    );
+    console.log(`${number} new contacts added successfully!`);
+    console.log(`Общее количество контактов теперь: ${updatedContacts.length}`);
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
 
-generateContacts(5);
+generateContacts(3);
